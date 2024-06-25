bbfcff1256ae270e72bf34c5ab8e168aad54cc15
PaulPak <paul.Pakkapol@gmail.com>
2024-06-25 15:24:37 +0700

    Menu
    Add menu and credit scene including scene transition

---
+++ Assets/Scripts/UXUI/SceneController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+       SceneManager.LoadSceneAsync(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
